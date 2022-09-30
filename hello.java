f8690de4e949fc04af049578af05b5d40516a07f
Raj <tiwarihomeone@gmail.com>
2022-09-30 14:46:47 +0530

    updated for trigger

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 class hello {
     public static void main(String[] args) {
+        System.out.println("cheecking trigger "); 
         System.out.println("hello world once again. Raj Tiwari here."); 
     }
 }
